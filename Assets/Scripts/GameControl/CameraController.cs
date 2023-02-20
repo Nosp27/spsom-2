860f70115300e354b8180880193e14fb3413fd3d
p.ashikhmin <p.ashikhmin@rambler-co.ru>
2023-02-20 23:46:06 +0300

    Add followRotation to camera; Intrpolate Ship RBs

--- Assets/Scripts/GameControl/CameraController.cs
+++ Assets/Scripts/GameControl/CameraController.cs
@@ -9,6 +9,8 @@ public class CameraController : MonoBehaviour
     private Vector3 CameraOffset => BaseOffset * Zoom;
     private Transform attachedListener;
 
+    [SerializeField] private bool followRotation;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,7 +27,8 @@ public class CameraController : MonoBehaviour
         transform.position = Vector3.Lerp(
             transform.position, PlayerShip.transform.position + CameraOffset, 2f * Time.unscaledDeltaTime
         );
-        transform.LookAt(PlayerShip.transform);
+        if (followRotation)
+            transform.LookAt(PlayerShip.transform);
 
         // if (PlayerShip)
         // {
