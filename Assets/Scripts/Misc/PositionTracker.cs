f0bf194e874785256a48409a6e8ca89fe6344c8e
p.ashikhmin <p.ashikhmin@rambler-co.ru>
2023-06-21 00:13:30 +0300

    Add illumination effect

--- Assets/Scripts/Misc/PositionTracker.cs
+++ Assets/Scripts/Misc/PositionTracker.cs
@@ -1,14 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PositionTracker : MonoBehaviour
 {
     public Transform target;
+    [SerializeField] private bool captureOffset;
+    private bool m_OffsetCaptured;
+    private Vector3 m_Offset;
 
     void LateUpdate()
     {
-        if (target != null)
-            transform.position = target.position;
+        if (target == null)
+            return;
+        
+        if (!m_OffsetCaptured && captureOffset)
+        {
+            m_Offset = transform.position - target.position;
+            m_OffsetCaptured = true;
+        }
+        transform.position = target.position + m_Offset;
     }
 }
